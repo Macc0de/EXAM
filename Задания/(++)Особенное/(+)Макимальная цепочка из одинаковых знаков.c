d806551eb0e9eca1bba3b08a9fc734cc912f9a23
Macc0de <mg3595293@gmail.com>
2024-01-10 10:25:38 +0200

    t

---
+++ Задания/(++)Особенное/(+)Макимальная цепочка из одинаковых знаков.c
@@ -0,0 +1,30 @@
+int func()
+{
+	struct List* cur;
+	int cur_len = 1, max_len = 0;
+	
+	if(First == NULL)
+		return max_len;
+	
+	cur = First;
+	while(cur != NULL && cur->Next != NULL)
+	{
+		if((cur->Value >= 0 && cur->Next->Value >= 0) || (cur->Value < 0 && cur->Next->Value < 0))
+		{
+			cur_len++;
+		}
+		else
+		{
+			if(cur_len > max_len)
+				max_len = cur_len;
+			cur_len = 1; // Сброс
+		}
+		
+		cur = cur->Next;
+	}
+	
+	if(cur_len > max_len) // Последняя цепочка
+		max_len = cur_len;
+	
+	return max_len;
+}
